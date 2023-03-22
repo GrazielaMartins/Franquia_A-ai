59afede00da76f50b5926eecf472b15415c3ea0a
Graziela Martins <116605840+GrazielaMartins@users.noreply.github.com>
2023-03-21 22:00:43 -0300

    Add files via upload

---
+++ Franquia_Acai/src/Acai.java
@@ -0,0 +1,33 @@
+public class Acai {
+    String tamanho;
+    Double valor;
+    Boolean acrescimoSorvete;
+
+    public double calcularValorAcai(double valor){
+        if (tamanho.equals("300ML")){
+            return valor;
+
+        }return valor=8.0;
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+    //public double calculaValor( boolean acrescimoSorvete){
+        //if (acrescimoSorvete){
+            //return +2.00;
+
+
+
+
